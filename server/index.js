ede1120a1fc7527a3ca761c82c4926fe9ae43c33
unknown <tkdgns25300@gmail.com>
2022-01-18 22:12:14 +0900

    connect database and server

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const app = express();
 const cors = require('cors') ;
+const pool = require('./db.js')
 const PORT = 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
 
 app.listen(PORT, () => {
     console.log(`Serer has started on port ${PORT}`)
