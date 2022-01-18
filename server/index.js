09e46477c45e268cd5859f1940eda947993e27cf
unknown <tkdgns25300@gmail.com>
2022-01-18 21:57:37 +0900

    server setup

--- server/index.js
+++ server/index.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const app = express();
+const cors = require('cors') ;
 const PORT = 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
 
 app.listen(PORT, () => {
     console.log(`Serer has started on port ${PORT}`)
