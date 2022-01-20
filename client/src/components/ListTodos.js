26df939793b2b82aefe64221b30774c7c5c1a291
unknown <tkdgns25300@gmail.com>
2022-01-20 23:17:56 +0900

    build ListTodo component

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -1,6 +1,23 @@
 import React, { Fragment, useEffect, useState } from "react";
 
 const ListTodos = () => {
+
+    const [todos, setTodos] = useState([]);
+
+    const getTodos = async() => {
+        try {
+            const response = await fetch("http://localhost:5000/todos");
+            const jsonData = await response.json();
+            setTodos(jsonData);
+        } catch (err) {
+            console.error(err.message);
+        }
+    }
+
+    useEffect(() => {
+        getTodos();
+    }, [])
+
     return (
         <Fragment>
             <table className="table mt-5 text-center">
@@ -17,6 +34,15 @@ const ListTodos = () => {
                         <td>Doe</td>
                         <td>john@example.com</td>
                     </tr> */}
+                    {todos.map(todo => {
+                        return (
+                            <tr>
+                                <td>{todo.description}</td>
+                                <td>Edit</td>
+                                <td>Delte</td>
+                            </tr>
+                        )
+                    })}
                 </tbody>
             </table>
         </Fragment>
