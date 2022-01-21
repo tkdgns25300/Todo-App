c9496be44bacf5cbdb4733b7dcfa7b5bec23b4b0
unknown <tkdgns25300@gmail.com>
2022-01-21 21:44:18 +0900

    complete inputTodo component

--- client/src/components/ListTodos.js
+++ client/src/components/ListTodos.js
@@ -4,6 +4,18 @@ const ListTodos = () => {
 
     const [todos, setTodos] = useState([]);
 
+    // delete todo function
+    const deleteTodo = async(id) => {
+        try {
+            const deleteTodo = await fetch(`http://localhost:5000/todos/${id}`, {
+                method: "DELETE"
+            })
+            setTodos(todos.filter(todo => todo.todo_id !== id))
+        } catch(err) {
+            console.error(err.message);
+        }
+    }
+
     const getTodos = async() => {
         try {
             const response = await fetch("http://localhost:5000/todos");
@@ -36,10 +48,12 @@ const ListTodos = () => {
                     </tr> */}
                     {todos.map(todo => {
                         return (
-                            <tr>
+                            <tr key={todo.todo_id}>
                                 <td>{todo.description}</td>
                                 <td>Edit</td>
-                                <td>Delte</td>
+                                <td>
+                                    <button className="btn btn-danger" onClick={() => deleteTodo(todo.todo_id)}>Delete</button>
+                                </td>
                             </tr>
                         )
                     })}
